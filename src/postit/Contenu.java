35f4202709a5173908aeb4bda348a1a1cf62ddb0
Deveaux Duncan <ddeveaux@localhost.localdomain>
2017-01-11 08:46:30 +0100

    Contenu corrigé

--- src/postit/Contenu.java
+++ src/postit/Contenu.java
@@ -1,5 +1,8 @@
 package postit;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Contenu
 {
 	private List<EltMultimedia> contenu;
@@ -8,15 +11,22 @@ public class Contenu
 		this.contenu = new ArrayList<EltMultimedia>();
 	}
 	
-	public addContenu(EltMultimedia){
-		this.contenu.add(EltMultimedia);
+	public void addElement(EltMultimedia elt){
+		this.contenu.add(elt);
+	}
+	
+	public List<EltMultimedia> getElements(){
+		return this.contenu;
 	}
 	
 	public String toHTML() {
-		String html;
+		String html = "<div>";
 		for(EltMultimedia e : contenu){
 			html+=e.toHtml();
 		}
+		
+		html += "</div>";
+		
 		return html;
 	}
 	
